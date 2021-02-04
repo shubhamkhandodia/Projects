5fcb02f51d265120db71da5f9d30420968f04065
unknown <shubhamkhandodia27@gmail.com>
2021-02-04 10:56:55 +0530

    Added dispatch actions to react-redux-form to preserve the state even when we navigate to some other tab

--- React/course_app/src/Components/MainComponent.js
+++ React/course_app/src/Components/MainComponent.js
@@ -10,6 +10,7 @@ import About from './AboutComponent'
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import {connect} from 'react-redux'
 import { addComment, fetchDishes } from '../redux/ActionCreators'
+import { actions } from 'react-redux-form';
 
 
 
@@ -25,7 +26,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => ({
 
   addComment: (dishId , rating , author , comment) => dispatch(addComment(dishId , rating , author , comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 });
 
 
@@ -79,7 +81,7 @@ class MainComponent extends React.Component {
                 <Switch>
                   <Route path="/home" component = {Homepage} />
                   <Route exact path="/menu" component = {() => <Menu mydishes={this.props.mydishes.dishes} />} />
-                  <Route exact path = "/contactus" component = {Contact} />
+                  <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                   <Route exact path = "/aboutus" component = {Aboutpage} />
                   <Route path='/menu/:dishId' component={DishWithId} />
                   <Redirect to='/home' />
--- React/course_app/src/redux/configureStore.js
+++ React/course_app/src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import Leaders from './leadersreducer';
 import Comments from './commentsreducer';
 import Promotions from './promotionsreducer';
 import Dishes from './dishreducer'
+import InitialFeedback from './formsreducer';
 
 
 export const Configurestore = () => {
@@ -13,7 +15,10 @@ export const Configurestore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
